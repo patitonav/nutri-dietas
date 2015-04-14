ff7076d5d0451853e120a2b181e81a90eba9d866
patitonav <pato.lalala@gmail.com>
2015-04-13 21:02:45 -0300

    Diets selection

--- app/controllers/diets_controller.rb
+++ app/controllers/diets_controller.rb
@@ -3,6 +3,34 @@ class DietsController < ApplicationController
   before_action :authenticate_user!
 
   def show
+  	@diets_set = [];
+  	if (current_user.diet.fecal_incontinence ) 
+  		@diets_set.push("fecal_incontinence");
+  	end
+  	if (current_user.diet.hemacromotosis)
+  		@diets_set.push("hemacromotosis");
+  	end
+  	if (current_user.diet.celiac)
+  		@diets_set.push("celiac");
+  	end
+  	if (current_user.diet.irritable_colon)
+  		@diets_set.push("irritable_colon");
+  	end
+  	if (current_user.diet.pregnancy) 
+  		@diets_set.push("pregnancy");
+  	end
+  	if (current_user.diet.ovolact) 
+  		@diets_set.push("ovolact");
+  	end
+  	if (current_user.diet.kosher) 
+  		@diets_set.push("kosher");
+  	end
+  	if (current_user.diet.without_fibers) 
+  		@diets_set.push("without_fibers");
+  	end
+  	if (current_user.diet.allowed_foods) 
+  		@diets_set.push("allowed_foods");
+  	end
   end
 
   private
